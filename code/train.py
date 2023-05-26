e4d2ddac9c84d21e0a0d9f1632769c8e08b5f483
Mayla Kersten <mkersten@int4.local.snellius.surf.nl>
2023-05-26 18:46:42 +0200

    update

--- code/train.py
+++ code/train.py
@@ -30,19 +30,10 @@ def compute_metrics(eval_pred):
     metrics = metric.compute(predictions=predictions, references=labels)
     return metrics
 
-def compute_loss(self, model, inputs, return_outputs=False):
-    labels = inputs.get("labels")
-    # forward pass
-    outputs = model(**inputs)
-    logits = outputs.get("logits")
-
-    loss_fct = nn.CrossEntropyLoss()
-    loss = loss_fct(logits.view(-1, 2), labels.view(-1))
-    return (loss, outputs) if return_outputs else loss
 
 if __name__ == "__main__":
     print("LOAD DATA")
-    print("------------------------------------------------------------------------")
+    print("------------------------------------------------------------------------\n")
 
     parser = argparse.ArgumentParser(description="Training")
     parser.add_argument('-m', '--mode', choices=['t', 'a'], help="Select mode: 't' for text-based, 'a' for argumentation-based")
@@ -54,7 +45,8 @@ if __name__ == "__main__":
     if args.mode == "t":
         mode = "text-based"
 
-    print(mode)
+    print(f"MODE {mode}")
+    print("------------------------------------------------------------------------\n")
 
     metric = evaluate.combine(["accuracy", "f1", "precision", "recall"])
 
@@ -62,44 +54,46 @@ if __name__ == "__main__":
         current_time = datetime.now()
 
         print(f"TRAINING: {model_name} - START: {current_time.hour}:{current_time.minute}")
-        print("------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------\n")
 
         dir = f"pipeline/{mode}/data"
         for name in os.listdir(dir):
             if os.path.isdir(f"{dir}/{name}"):
                 train = pd.read_csv(f"{dir}/{name}/train.csv").dropna()
                 test = pd.read_csv(f"{dir}/{name}/test.csv").dropna()
-                # try:
-                #     # Load checkpoint
-                #     files = os.listdir(f"models/{mode}/{model_name}/{name}")
-                #     checkpoint_files = [f for f in files if f.startswith("checkpoint")]
-                #     if checkpoint_files:
-                #         sorted_checkpoint_files = sorted(checkpoint_files, key=lambda x: int(x.split("-")[1]), reverse=True)
-                #         highest_checkpoint = sorted_checkpoint_files[0]
-                #         model_path = f"models/text/{model_name}/{name}/" + highest_checkpoint 
-                # except FileNotFoundError:
-                #     model_path = model_name
-                print(f"LOADING: {model_name}")
 
-                if model_name == "google/electra-base-discriminator":
-                    tokenizer = ElectraTokenizer.from_pretrained(model_name, padding=True, truncation=True, return_tensors="pt")
-                    model = ElectraForSequenceClassification.from_pretrained(f"{model_name}", num_labels=2, id2label={0: "FAKE", 1: "REAL"}, label2id={"FAKE": 0, "REAL": 1})
-                else:
-                    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, return_tensors="pt") 
-                    model = AutoModelForSequenceClassification.from_pretrained(f"{model_name}", num_labels=2, id2label={0: "FAKE", 1: "REAL"}, label2id={"FAKE": 0, "REAL": 1})
-        
                 print(f"DATASET: {name} - LENGTH TRAIN: {len(train)}")
-                print("------------------------------------------------------------------------")
+                print("------------------------------------------------------------------------\n")
 
                 train = Dataset.from_pandas(train).class_encode_column("label")
                 test = Dataset.from_pandas(test).class_encode_column("label")
-
                 print(train, test)
+                print("------------------------------------------------------------------------\n")
+
+                print(f"LOADING: {model_name}")
+                print("------------------------------------------------------------------------\n")
+
+                try:
+                    # Load checkpoint
+                    files = os.listdir(f"models/{mode}/{model_name}/{name}")
+                    checkpoint_files = [f for f in files if f.startswith("checkpoint")]
+                    if checkpoint_files:
+                        sorted_checkpoint_files = sorted(checkpoint_files, key=lambda x: int(x.split("-")[1]), reverse=True)
+                        highest_checkpoint = sorted_checkpoint_files[0]
+                        model_path = f"models/text/{model_name}/{name}/" + highest_checkpoint 
+                except FileNotFoundError:
+                    model_path = model_name
+
+                if model_name == "google/electra-base-discriminator":
+                    tokenizer = ElectraTokenizer.from_pretrained(model_name, padding=True, truncation=True, return_tensors="pt")
+                    model = ElectraForSequenceClassification.from_pretrained(f"{model_path}", num_labels=2, id2label={0: "FAKE", 1: "REAL"}, label2id={"FAKE": 0, "REAL": 1})
+                else:
+                    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, return_tensors="pt") 
+                    model = AutoModelForSequenceClassification.from_pretrained(f"{model_path}", num_labels=2, id2label={0: "FAKE", 1: "REAL"}, label2id={"FAKE": 0, "REAL": 1})
+                data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
                 tokenized_train = train.map(preprocess_function, batched=True)
                 tokenized_test = train.map(preprocess_function, batched=True)
-
-                data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
                 # Documentation: https://huggingface.co/transformers/v3.0.2/main_classes/trainer.html
                 training_args = TrainingArguments(
@@ -122,6 +116,17 @@ if __name__ == "__main__":
     
                 early_stop = EarlyStoppingCallback(1, 0) # Zero because delta
 
+                class Classifyer(Trainer):
+                    def compute_loss(self, model, inputs, return_outputs=False):
+                        labels = inputs.get("labels")
+                        # forward pass
+                        outputs = model(**inputs)
+                        logits = outputs.get("logits")
+
+                        loss_fct = nn.CrossEntropyLoss()
+                        loss = loss_fct(logits.view(-1, 2), labels.view(-1))
+                        return (loss, outputs) if return_outputs else loss
+
                 trainer = Trainer(
                     model=model,
                     args=training_args,
@@ -129,7 +134,6 @@ if __name__ == "__main__":
                     train_dataset=tokenized_train,
                     eval_dataset=tokenized_test,
                     compute_metrics=compute_metrics,
-                    compute_loss=compute_loss,
                     callbacks=[early_stop]
                     )
                 
@@ -140,6 +144,6 @@ if __name__ == "__main__":
                 trainer.save_model()
                 trainer.evaluate()
                 writer.close()
-                print("------------------------------------------------------------------------")
+                print("------------------------------------------------------------------------\n")
                 print()
 
