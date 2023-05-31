d5d335eaa884c27f200b76ddb36dc2519facd202
Mayla Kersten <mkersten@int6.local.snellius.surf.nl>
2023-05-31 10:55:03 +0200

    updates

--- code/dolly.py
+++ code/dolly.py
@@ -1,33 +1,40 @@
-import pandas as pd
+import pandas as pd;
 import sys; print(sys.path)
 import torch; print(torch.__version__)
 from transformers import pipeline
 import os
 
 def extract_claim(text):
+    print(text)
     claim = generate_text(f"Retrieve the (implicit) claim(s) opted by the writer from the following news article: {text}, in case there are more than one, please separate them with '\n'. Do not add any other information than the claim(s).")
-    return claim
+    return claim[0]["generated_text"]
 
 def extract_evidence(text):
     evidence = generate_text(f"Retrieve the (implicit) evidences(s) opted by the writer from the following news article: {text}, in case there are more than one, please separate them with '\n'. Do not add any other information than the evidence(s).")
-    return evidence
+    return evidence[0]["generated_text"]
 
 if __name__ == "__main__":
     torch.cuda.empty_cache()
     print(f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
     generate_text = pipeline(model="databricks/dolly-v2-3b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
 
-    dir = f"pipeline/argumentation-based/data"
-    for name in os.listdir(dir):
-        train = pd.read_csv(f"{dir}/{name}/train.csv").dropna()
-        test = pd.read_csv(f"{dir}/{name}/test.csv").dropna()
+    dir = f"pipeline/argumentation-based"
+    for name in os.listdir(f"data"):
+        if name != ".DS_Store":
+            train = pd.read_csv(f"{dir}/data/{name}/train.csv").dropna()
+            test = pd.read_csv(f"{dir}/data/{name}/test.csv").dropna()
 
-        print(f"DATASET: {name} - LENGTH TRAIN: {len(train)}")
-        print("------------------------------------------------------------------------\n")
+            print(f"DATASET: {name} - LENGTH TRAIN: {len(train)}")
+            print("------------------------------------------------------------------------\n")
 
-        train["claim"] = train.apply(lambda x: extract_claim(x["text"]), axis=1)
-        train["evidence"] = train.apply(lambda x: extract_evidence(x["text"]), axis=1)
+            train["claim"] = train.apply(lambda x: extract_claim(x["text"]), axis=1)
+            train["evidence"] = train.apply(lambda x: extract_evidence(x["text"]), axis=1)
 
-        print(train.head())
-        train.to_csv(f"{dir}/argumentation structure/dolly/{name}/train.csv")
-        test.to_csv(f"{dir}/argumentation structure/dolly/{name}/test.csv")
+            p = f"{dir}/argumentation structure/dolly"
+            if not os.path.exists(p):
+                os.makedirs(f"{p}/train")
+                os.makedirs(f"{p}/test")
+
+            print(train.head())
+            train.to_csv(f"{p}/train/{name}.csv")
+            test.to_csv(f"{p}/test/{name}.csv")
--- code/margot.py
+++ code/margot.py
@@ -5,26 +5,27 @@ import json
 import numpy as np
 import shutil
 
-path = "pipeline/argumentation-based/tools/MARGOT/"
-tool = "pipeline/argumentation-based/tools/MARGOT/run_margot.sh"
+path = "pipeline/argumentation-based/tools/predictor"
+tool = "pipeline/argumentation-based/tools/predictor/run_margot.sh"
 
-def df_to_doc(clean_text):
+def df_to_doc(clean_text, type):
     global counter
-    with open(f"{path}/temp/news/{counter}.txt", "a+") as text_file:
+    print(f"Writing to {path}/temp/news/{type}")
+    with open(f"{path}/temp/news/{type}/{counter}.txt", "a+") as text_file:
         text_file.write(clean_text)
     counter += 1
     
-def extract_argumentation(name):
+def extract_argumentation(name, type):
     global step
-    for idx, file in enumerate(sorted(os.listdir(f"{path}/temp/news"), key=lambda x: int(x.split('.')[0]))):
+    for idx, file in enumerate(sorted(os.listdir(f"{path}/temp/news/{type}"), key=lambda x: int(x.split('.')[0]))):
+        print(idx, file)
 
-        total = len(os.listdir(f"{path}/temp/news"))
+        total = len(os.listdir(f"{path}/temp/news/{type}"))
         print(f"STEP: {step}")
         print("------------------------------------------------------------------------")
 
-
-        input = f"{name}/temp/news/{file}"
-        output = f"{name}/temp/arguments/{idx}"
+        input = f"temp/news/{type}/{file}"
+        output = f"temp/arguments/{type}/{idx}"
         subprocess.call([tool, input, output])
 
         step += 1
@@ -33,17 +34,19 @@ def extract_argumentation(name):
         if step in np.arange(0, total, 50):
             print("WRITE OUT TEMPORARY RESULTS")
             print("------------------------------------------------------------------------")
-            res = parse_output()
+            res = parse_output(type)
             temp_result = pd.DataFrame.from_dict(res, orient='index')
-            temp_result.to_csv(f"{path}/temp_results/temp_{name}")
+            if os.path.exists(f"{path}/argumentation structure/margot/temp_{name}"):
+                os.makedirs(f"{path}/argumentation structure/margot/temp_{name}")
+            temp_result.to_csv(f"{path}/argumentation structure/margot/temp_{name}/{type}.csv")
 
-    res = parse_output()
+    res = parse_output(type)
     return pd.DataFrame.from_dict(res, orient='index')
 
-def parse_output():
+def parse_output(type):
     argument_structures = {}
-    for dir in sorted(os.listdir(f"{path}/temp/arguments"), key=lambda x: int(x.split('.')[0])):
-        with open(f"{path}/temp/arguments/{dir}/OUTPUT.json") as json_file:
+    for dir in sorted(os.listdir(f"{path}/temp/arguments/{type}"), key=lambda x: int(x.split('.')[0])):
+        with open(f"{path}/temp/arguments/{type}/{dir}/OUTPUT.json") as json_file:
 
             doc = json.loads(json_file.read())
             args = {"claim": [], "evidence": []}
@@ -62,7 +65,7 @@ def parse_output():
 
 if __name__ == "__main__":
     dir = f"pipeline/argumentation-based/data"
-    for name in os.listdir(dir):    
+    for name in os.listdir(dir):  
         if os.path.isdir(f"{dir}/{name}"):
             train = pd.read_csv(f"{dir}/{name}/train.csv").dropna()
             test = pd.read_csv(f"{dir}/{name}/test.csv").dropna()
@@ -70,19 +73,32 @@ if __name__ == "__main__":
             step = 0
             counter = 0
 
-            dir = f"{path}/temp"
-            if os.path.exists(dir):
-                shutil.rmtree(dir)
-            os.makedirs(f"{dir}/news", f"{dir}/arguments")
+            path_to_temp_dir = f"pipeline/argumentation-based/tools/predictor/temp"
+            if os.path.exists(path_to_temp_dir):
+                shutil.rmtree(path_to_temp_dir)
+            os.makedirs(f"{path_to_temp_dir}/news/train")
+            os.makedirs(f"{path_to_temp_dir}/news/test")
+
+            os.makedirs(f"{path_to_temp_dir}/arguments/train")
+            os.makedirs(f"{path_to_temp_dir}/arguments/test")
 
             # Convert news values out to documents
-            train.apply(lambda x: df_to_doc(x["text"]), axis=1)
-            test.apply(lambda x: df_to_doc(x["text"]), axis=1)
+            train.apply(lambda x: df_to_doc(x["text"], type="train"), axis=1)
+            test.apply(lambda x: df_to_doc(x["text"], type="test"), axis=1)
 
-            # Run Margot over the documents
-            train_result = extract_argumentation(train, name)
-            test_result = extract_argumentation(test, name)
+            # # Run Margot over the documents
+            train_result = extract_argumentation(name, "train")
+            test_result = extract_argumentation(name, "train")
+
+            os.makedirs(f"{path}/argumentation structure/margot/train")
+            os.makedirs(f"{path}/argumentation structure/margot/test")
+
+            path_to_train = f"{os.getcwd()}/pipeline/argumentation-based/argumentation structure/margot/train"
+            path_to_test = f"{os.getcwd()}/pipeline/argumentation-based/argumentation structure/margot/test"
+            if not os.path.exists(path_to_temp_dir):
+                os.makedirs(path_to_train)
+                os.makedirs(path_to_test)
 
             # Write result data/argumentation_structure
-            train_result.to_csv(f"{dir}/argumentation structure/margot/{name}/train.csv")
-            test_result.to_csv(f"{dir}/argumentation structure/margot/{name}/test.csv")
+            train_result.to_csv(f"{path_to_train}/{name}.csv")
+            test_result.to_csv(f"{path_to_test}/{name}.csv")
